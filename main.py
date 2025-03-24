b1cbe978adbbd89914109658135828485ae0b173
brenoportella_ <bp.bohry@gmail.com>
2025-03-24 15:13:17 +0000

    first working version

---
+++ main.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+from modules.driver import quit_driver, setup_driver
+from modules.login import login
+
+df = pd.read_excel('company_list.xlsx')
+driver = setup_driver()
+i = 1
+df['STATUS'] = ''
+
+for index, row in df.iterrows():
+
+    nif = row['NIF']
+    password = row['PASSWORD']
+
+    print(f'Tentando login para NIF: {nif}')
+    status = login(driver, nif, password)
+    df.at[index, 'STATUS'] = status
+    i += 1
+    if i % 10 == 0:
+        df.to_excel('company_list_output.xlsx', index=False)
+
+df.to_excel('company_list_output.xlsx', index=False)
+print('Processo concluído. Status salvo no Excel.')
+quit_driver(driver)
